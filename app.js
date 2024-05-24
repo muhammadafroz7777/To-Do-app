97638f1e8b4ae040247dc846218478b9fa81db0d
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-05-24 19:59:50 +0500

    11:46 conti

---
+++ app.js
@@ -0,0 +1,32 @@
+// var parrent = document.getElementById("parrent");
+// var Child1 = document.getElementById("child1");
+// // parrent ko hasil krne k leye
+// console.log(parrent); 
+
+
+
+// // Child1 ko hasil krne k leye
+// console.log("Child1 =>" ,Child1);
+
+
+// // parrent k kitne child hain  hasil krne k leye
+// console.log(parrent.children); 
+
+
+// // Child1 ka parrent hasil krne k leye
+// console.log("Child1 ka parent =>" ,Child1.parentElement);
+
+// // Child1 ka Sibling hasil krne k leye
+// console.log("Child1 ka Sibling =>" ,Child1.previousElementSibling);
+// console.log("Child1 ka Sibling =>" ,Child1.nextElementSibling);
+
+
+var todo_input = document.getElementById("todo_input");   
+var add_todo_btn = document.getElementById("add_todo_btn");   
+var delete_all_todo_btn = document.getElementById("delete_all_todo_btn");   
+var todo_list = document.getElementById("todo_list");   
+
+add_todo_btn.addEventListener('click' , function() {
+    
+})
+
