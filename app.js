3bc0b51f6130969e29307d6c5c17838d6a66aee5
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-05-25 23:10:25 +0500

    should help

--- app.js
+++ app.js
@@ -23,7 +23,7 @@
 
 var todo_input = document.getElementById("todo_input");   
 var add_todo_btn = document.getElementById("add_todo_btn");   
-var delete_all_todo_btn = document.getElementById("delete_all_todo_btn");   
+// var delete_all_todo_btn = document.getElementById("delete_all_todo_btn");   
 var todo_list = document.getElementById("todo_list");   
 
 add_todo_btn.addEventListener('click' , function() {
@@ -31,7 +31,7 @@ add_todo_btn.addEventListener('click' , function() {
 if (!todo_input.value) return alert('Tdo main kuch daaldo') 
     
 
-    var list_item = `<li><span>${todo_input.value}</span><button onclick="edit(this)">Edit</button><button onclick="deleteFun(this)">del</button></li>`
+    var list_item = `<li><span>${todo_input.value}</span> <button class = "edit_btn"  onclick="edit(this)">Edit</button> <button class = 'del_btn' onclick="deleteFun(this)">Del</button></li>`
 
     todo_list.innerHTML += list_item;
     todo_input.value = '';
@@ -40,12 +40,17 @@ if (!todo_input.value) return alert('Tdo main kuch daaldo')
 })
 
 function edit(element) {
-    console.log("edit =>" , element.previousElementSibling.innerText);
-    var previousValue = 
-}
+    // console.log("edit =>" , element.previousElementSibling.innerText);
+    var previousValue = element.previousElementSibling.innerText;
+    var updatedValue  = prompt('Edit Value', previousValue);
+    element.previousElementSibling.innerText = updatedValue ;
+} 
 
 function deleteFun(element) {
+
     
     element.parentElement.remove();
     
 }
+
+
